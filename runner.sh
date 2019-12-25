79630bd607e11bfb12ef7984dc9c695db5ebec20
Witold Szczerba <witoldsz.dev@gmail.com>
2019-12-26 00:25:34 +0100

    Intial commit as a project

--- runner.sh
+++ runner.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$(dirname "$0")"
 NODE_BIN=$(find . -maxdepth 3 -path '*/bin/node')
+NODE_BIN=${NODE_BIN:-node}
 LOG_FILE=./log.txt
 
 reboot=$($NODE_BIN ./status-processor.js | tee -a $LOG_FILE | grep REBOOT)
